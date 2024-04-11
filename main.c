d443ef93b732d2827f5af571b054350788b5781b
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-04-10 22:01:16 -0300

    criada a fução pilha

--- main.c
+++ main.c
@@ -17,3 +17,32 @@ typedef struct {
     int topo;
 } PilhaPratos;
 
+//agora vou criar as funções de enfileirar e empilhar
+
+void inicializarFila(FilaPratos *fila) {
+    fila->frente = 0;
+    fila->fundo = -1;
+    fila->quantidade = 0;
+}
+void enfileirarPrato(FilaPratos *fila, char *prato) {
+    if (fila->quantidade == MAX_TAM) {
+        printf("Já deu Rosa, não tem mais espaço.. Não é possível enfileirar mais pratos.\n");
+        return;
+    }
+    fila->fundo = (fila->fundo + 1) % MAX_TAM;
+    strcpy(fila->pratos[fila->fundo], prato);
+    fila->quantidade++;
+}
+//desfileirando 
+
+char* desenfileirarPrato(FilaPratos *fila) {
+    if (fila->quantidade == 0) {
+        printf("Erro: A fila de pratos está vazia. Não é possível desenfileirar mais pratos.\n");
+        return NULL;
+    }
+    char *prato = fila->pratos[fila->frente];
+    fila->frente = (fila->frente + 1) % MAX_TAM;
+    fila->quantidade--;
+    return prato;
+}
+
