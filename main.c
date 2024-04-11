ed608391efe63113649177c0a9e039d818956ef7
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-04-11 06:36:39 -0300

    Projeto final

--- main.c
+++ main.c
@@ -45,4 +45,65 @@ char* desenfileirarPrato(FilaPratos *fila) {
     fila->quantidade--;
     return prato;
 }
+void inicializarPilha(PilhaPratos *pilha) {
+    pilha->topo = -1;
+}
+
+void empilharPrato(PilhaPratos *pilha, char *prato) {
+    if (pilha->topo == MAX_TAM - 1) {
+        printf("Já deu mano, não cabe mais pratos, vamos enxaguar..\n");
+        return;
+    }
+    pilha->topo++;
+    strcpy(pilha->pratos[pilha->topo], prato);
+}
+
+void desempilharPrato(PilhaPratos *pilha) {
+    if (pilha->topo == -1) { 
+        printf("Erro: Pilha vazia. Não é possível remover mais pratos.\n");
+        return;
+    }
+    printf("Prato enxaguado: %s\n", pilha->pratos[pilha->topo]);
+    pilha->topo--; 
+}
+
+int main() {
+    FilaPratos fila;
+    PilhaPratos pilha;
+
+    inicializarFila(&fila);
+    inicializarPilha(&pilha);
+
+    printf("Rosa, o que acha de lavar alguns pratos?\n");
+    printf("Vamos começar a lavar os pratos...\n\n");
+
+    for (int i = 0; i < MAX_TAM; i++) {
+        char corPrato[20];
+        printf("Informe a cor do prato %d: ", i + 1);
+        scanf("%s", corPrato);
+
+        enfileirarPrato(&fila, corPrato);
+
+        printf("Prato %d (%s) enfileirado.\n\n", i + 1, corPrato);
+    }
+
+    printf("Todos os pratos enfileirados. Agora vamos empilhá-los...\n\n");
+
+    while (fila.quantidade > 0) {
+        char *prato = desenfileirarPrato(&fila);
+        empilharPrato(&pilha, prato);
+        printf("Prato %s empilhado.\n\n", prato);
+    }
+
+    printf("Todos os pratos empilhados. Agora vamos enxaguá-los...\n\n");
+
+    while (pilha.topo >= 0) {
+        desempilharPrato(&pilha);
+    }
+
+    printf("Cara, você arrasou. Louça zerada!\n");
+
+    return 0;
+}
+
 
